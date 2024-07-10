76290503907a6cf049ea318e2d757f443d59863a
isomd <3250162035@qq.com>
2024-07-10 16:20:54 +0800

    init

---
+++ src/main/java/tml/xiaochun/cicd_test/CiCdTestApplication.java
@@ -0,0 +1,13 @@
+package tml.xiaochun.cicd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdTestApplication.class, args);
+    }
+
+}
